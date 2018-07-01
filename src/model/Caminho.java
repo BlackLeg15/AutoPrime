2cd00da5c31797f12177ae609a40269b4af0027b
Adby Santos <adbyocara_jprn@hotmail.com>
2018-07-01 14:45:22 -0300

    Classes TrocaTela e Caminho

---
+++ src/model/Caminho.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Caminho {
+
+    public static String telaLogin = "../view/TelaLogin.fxml";
+    public static String telaPrincipal = "../view/TelaPrincipal.fxml";
+    public static String telaRelatorios = "../view/TelaRelatorios.fxml";
+    public static String telaAlerta  = "../view/alert/TelaAlerta.fxml";
+
+}

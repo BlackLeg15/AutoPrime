c23b6026aa22b64330a1415e56ba600c05b88998
Liliane Rodrigues <liliane_pdf@hotmail.com>
2018-07-13 15:24:11 -0300

    Nova alteração

--- src/model/Caminho.java
+++ src/model/Caminho.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Caminho {
-
+		//testando
     public static String telaLogin = "../view/TelaLogin.fxml";
     public static String telaPrincipal = "../view/TelaPrincipal.fxml";
     public static String telaRelatorios = "../view/TelaRelatorios.fxml";

49b3bdc5bd75dc3aa9d6fcf91a323ac89e7caaab
Liliane Rodrigues <liliane_pdf@hotmail.com>
2018-07-31 19:36:48 -0300

    Edição de tela de Login

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,21 +1,93 @@
 package controller;
 
 import model.Caminho;
+
+import java.io.IOException;
+
 import model.Especificacoes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainController extends Application {
+	private Stage primaryStage;
+    private BorderPane rootLayout;
 
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+        
+        
+    }
+
+    /**
+     * Inicializa o root layout (layout base).
+     */
+    public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainController.class.getResource("/view/TelaLogin.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Mostra a scene (cena) contendo o root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mostra o person overview dentro do root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Carrega o person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainController.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Define o person overview dentro do root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna o palco principal.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
+    
+}
+	
+	/*
     @Override
     public void start (Stage primaryStage) throws Exception
     {
-        /**Esta classe chama a construção da primeira interface do software Scout, que desencadeia nas outras chamadas
+        *Esta classe chama a construção da primeira interface do software Scout, que desencadeia nas outras chamadas
          * de interfaces utilizando a tecnologia JavaFx
-         */
+         
 
         Parent root = FXMLLoader.load(getClass().getResource(Caminho.telaLogin));
 
@@ -32,5 +104,4 @@ public class MainController extends Application {
     public static void main (String[] args)
     {
         launch(args);
-    }
-}
+    }*/

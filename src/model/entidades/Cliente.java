7b36f0b9684245a76a270c500938d4eb55f107dd
Adby Santos <adbyocara_jprn@hotmail.com>
2018-08-14 00:09:21 -0300

    GeneratedValue nos IDs das entidades

--- src/model/entidades/Cliente.java
+++ src/model/entidades/Cliente.java
@@ -1,10 +1,18 @@
 package model.entidades;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -12,6 +20,7 @@ import javax.persistence.Table;
 public class Cliente extends Pessoa {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "IDPessoaCliente")
 	private int idPessoaCliente;
 
@@ -23,6 +32,14 @@ public class Cliente extends Pessoa {
 		super(nome, cpf, rg, sexo, dataNascimento, rua, numCasa, bairro, cidade, estado, telefone);
 		this.cnh = cnh;
 	}
+	
+	@ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+        name = "SolicitaCarro", 
+        joinColumns = { @JoinColumn(name = "IDPessoaCliente") }, 
+        inverseJoinColumns = { @JoinColumn(name = "IDCarro") }
+    )
+    Set<Carro> Carros = new HashSet<>();
 
 	public String getCnh() {
 		return cnh;
